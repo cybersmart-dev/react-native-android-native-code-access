35ca6ab8bed965c63a39cb9f4e0cd7458fa31ec4
cybersmart-dev <cybersmart83@gmail.com>
2025-09-22 15:24:13 +0100

    login ui

--- App.js
+++ App.js
@@ -1,25 +1,112 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NativeModules } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    Dimensions,
+    Button,
+    Image,
+    TouchableOpacity
+} from "react-native";
+//import { NativeModules } from 'react-native';
 
-const { ToastExample } = NativeModules;
-
-ToastExample.show("Hello from Native Android!");
+const { height, width } = Dimensions.get("window");
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View
+                style={{
+                    width: width,
+                    alignItems: "center",
+                    flexDirection: "column"
+                }}
+            >
+                <View style={styles.form}>
+                    <Image
+                        style={{
+                            height: 100,
+                            width: 100,
+                            textAlign: "center",
+                            borderRadius: 100
+                        }}
+                        source={require("./assets/profile_avatar.png")}
+                    />
+                </View>
+                <View
+                    style={{
+                        marginBottom: 40
+                    }}
+                >
+                    <Text style={styles.title}>Welcome Back</Text>
+                    <Text style={{ textAlign: "center", color: '#f7bc28', }}>
+                        Enter your password
+                    </Text>
+                </View>
+                {/** <TextInput style={styles.input} placeholder="Username" /> **/}
+                <TextInput style={styles.inputps} placeholder="Password" />
+                
+                 <TouchableOpacity style={{ alignSelf: 'left', marginLeft: 15, marginTop:10}}>
+                  <Text style={{ color: 'black',}}>Forgot Password?</Text>
+                </TouchableOpacity>
+                
+                <TouchableOpacity style={styles.button}>
+                  <Text style={{ color: 'white',}}>Login</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={{ marginTop: 20,}}>
+                  <Text style={{ color: 'black',}}>Switch Account</Text>
+                </TouchableOpacity>
+            </View>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "centcer",
+        justifyContent: "flex-start",
+        marginTop: height / 4
+    },
+    input: {
+        height: 50,
+        borderColor: "#f7bc28",
+        borderWidth: 1,
+        borderRadius: 6,
+        marginBottom: 25,
+        width: width - 30,
+        paddingLeft: 10,
+    },
+    inputps: {
+        height: 50,
+        borderColor: "#f7bc28",
+        borderWidth: 1,
+        borderRadius: 6,
+        
+        paddingLeft: 10,
+        width: width - 30
+    },
+    form: {
+        paddingHorizontal: 15
+    },
+    title: {
+        textAlign: "center",
+        fontSize: 25,
+        fontWeight: "bold",
+        color: '#f7bc28',
+    },
+    button:{
+        height: 40,
+        width: width/1.5,
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: '#f7bc28',
+        justifyContent: 'center',
+        borderRadius: 6,
+        marginTop: 25,
+        
+    }
 });
